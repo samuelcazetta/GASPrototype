f6e6f4b1e774c212d89c041f25b7bb306f7dc3c8
samuelcazetta <samuel.cazetta@gmail.com>
2026-05-15 15:22:34 -0300

    chore: ignore IDE files and add project note

--- Source/GASPrototype/GASPrototype.h
+++ Source/GASPrototype/GASPrototype.h
@@ -1,5 +1,9 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+//May the good Spirits guide me,
+//that I may have fun while doing this,
+//and that I may fulfill my heart while learning.
+
 #pragma once
 
 #include "CoreMinimal.h"
